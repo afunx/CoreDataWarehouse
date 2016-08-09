b5592ac0c410e9a7db4089b0c02de8f439da2274
afunx <afun1989@gmail.com>
2016-08-09 16:51:04 +0800

    CoreDataWarehouse is created

---
+++ CoreDataWarehouse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreDataWarehouse
+//
+//  Created by 白 桦 on 8/9/16.
+//  Copyright © 2016 白 桦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
